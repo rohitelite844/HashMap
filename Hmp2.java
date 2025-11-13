95f3cda2960e71ddb08d1163afd1238f7948defc
Rohit Ingle <rohitingle126@gmail.com>
2025-11-13 16:23:59 +0530

    Add files via upload

---
+++ Hmp2.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Hmp2 {
+
+	public static void main(String[] args) {
+    HashMap<Integer,String> map = new HashMap<Integer,String>();
+		
+		map.put(1,"Red");
+		map.put(2,"Green");
+		map.put(3,"Blue");
+		map.put(4,"Orange");
+		
+		System.out.println(map);
+		System.out.println("Size of Hashmap : "+map.size());
+		
+
+	}
+
+}
