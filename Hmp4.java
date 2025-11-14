66f9ad1777d5c326307d7f63796345b639483d60
Rohit Ingle <rohitingle126@gmail.com>
2025-11-14 13:08:24 +0530

    Add files via upload

---
+++ Hmp4.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Hmp4 {
+
+	public static void main(String[] args) {
+		 HashMap<Integer,String> map = new HashMap<Integer,String>();
+			
+			map.put(1,"Red");
+			map.put(2,"Green");
+			map.put(3,"Blue");
+			map.put(4,"Orange");
+			
+			System.out.println("Original map"+map);
+			map.clear();
+			System.out.println("The new map"+map);
+	}
+
+}
