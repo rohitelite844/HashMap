2859be07548e7522508fc5e1ba7a35d322826476
Rohit Ingle <rohitingle126@gmail.com>
2025-11-15 12:11:07 +0530

    Add files via upload

---
+++ Hmp6.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Hmp6 {
+
+	public static void main(String[] args) {
+    HashMap<Integer,String> map = new HashMap<Integer,String>();
+		
+		map.put(1,"Red");
+		map.put(2,"Green");
+		map.put(3,"Blue");
+		map.put(4,"Orange");
+		
+		System.out.println(map);
+		
+		System.out.println("Cloned map : "+map.clone());
+		
+		
+	}
+
+}
