f4ca03468dd32f9fd36014b411e290bf4e9c93b8
Rohit Ingle <rohitingle126@gmail.com>
2025-11-19 15:59:41 +0530

    Add files via upload

---
+++ Hmp12.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Hmp12 {
+
+	public static void main(String[] args) {
+HashMap<Integer,String> map = new HashMap<Integer,String>();
+		
+		map.put(1,"Red");
+		map.put(2,"Green");
+		map.put(3,"Blue");
+		map.put(4,"Orange");
+		
+		System.out.println(map);
+		System.out.println("Keys set values are : "+map.values());
+
+
+	}
+
+}
