1b501d063e123b65d62b2dabad64218d22f9b0a6
Rohit Ingle <rohitingle126@gmail.com>
2025-11-19 15:59:21 +0530

    Add files via upload

---
+++ Hmp11.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Hmp11 {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> map = new HashMap<Integer,String>();
+		
+		map.put(1,"Red");
+		map.put(2,"Green");
+		map.put(3,"Blue");
+		map.put(4,"Orange");
+		
+		System.out.println(map);
+		System.out.println("Keys set values are "+map.keySet());
+
+	}
+
+}
